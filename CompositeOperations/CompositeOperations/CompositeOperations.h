585707d6e76a24de726302c67da382d6dbe5d73a
Stanislaw Pankevich <s.pankevich@gmail.com>
2016-01-16 17:54:53 +0100

    Initial commit

---
+++ CompositeOperations/CompositeOperations/CompositeOperations.h
@@ -0,0 +1,19 @@
+//
+//  CompositeOperations.h
+//  CompositeOperations
+//
+//  Created by Stanislaw Pankevich on 16/01/16.
+//  Copyright © 2016 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CompositeOperations.
+FOUNDATION_EXPORT double CompositeOperationsVersionNumber;
+
+//! Project version string for CompositeOperations.
+FOUNDATION_EXPORT const unsigned char CompositeOperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CompositeOperations/PublicHeader.h>
+
+
